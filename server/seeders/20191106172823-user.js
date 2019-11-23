d2ee53612d658c3cf5ecc3cf5ec32e5c2168b6e8
jerryhuang3 <jerryhuang@utexas.edu>
2019-11-23 03:32:05 -0500

    add generated seed data

--- server/seeders/20191106172823-user.js
+++ server/seeders/20191106172823-user.js
@@ -1,33 +1,20 @@
-"use strict";
+'use strict';
+const mock = require('../mockUsers.json');
 
 module.exports = {
 	up: (queryInterface, Sequelize) => {
 		// seed test user
-		const users = [
-			{
-				username: "testuser",
-				firstName: "Nick",
-				lastName: "Saban",
-				email: "test@test.com",
-				password: "$2b$10$xJa4xYq7WvnA1d63TDSar.qPE87u21VXatJ8nHRi8g9HxTyWn7f56",
+		const users = mock.map(user => {
+			return {
+				...user,
 				createdAt: new Date(),
-				updatedAt: new Date()
-			},
-			{
-				username: "testuser2",
-				firstName: "Limp",
-				lastName: "Bizkit",
-				email: "test2@test.com",
-				password: "$2b$10$xJa4xYq7WvnA1d63TDSar.qPE87u21VXatJ8nHRi8g9HxTyWn7f56",
-				createdAt: new Date(),
-				updatedAt: new Date()
-			}
-		];
-
-		return queryInterface.bulkInsert("users", users, {});
+				updatedAt: new Date(),
+			};
+		});
+		return queryInterface.bulkInsert('users', users, {});
 	},
 
 	down: (queryInterface, Sequelize) => {
-		return queryInterface.bulkDelete("users", null, {});
-	}
+		return queryInterface.bulkDelete('users', null, {});
+	},
 };
--- server/seeders/20191114105243-favorite.js
+++ server/seeders/20191114105243-favorite.js
@@ -1,57 +1,20 @@
-"use strict";
+'use strict';
+const mock = require('../mockFavorites.json');
 
 module.exports = {
 	up: (queryInterface, Sequelize) => {
 		// seed test favorites
-		const favorites = [
-			{
-				userId: 1,
-				recipeId: 1,
-				favorited: 1,
+		const favorites = mock.map(favorite => {
+			return {
+				...favorite,
 				createdAt: new Date(),
-				updatedAt: new Date()
-			},
-			{
-				userId: 1,
-				recipeId: 2,
-				favorited: 0,
-				createdAt: new Date(),
-				updatedAt: new Date()
-			},
-			{
-				userId: 1,
-				recipeId: 3,
-				favorited: 1,
-				createdAt: new Date(),
-				updatedAt: new Date()
-			},
-			{
-				userId: 1,
-				recipeId: 7,
-				favorited: 1,
-				createdAt: new Date(),
-				updatedAt: new Date()
-			},
-			{
-				userId: 2,
-				recipeId: 3,
-				favorited: 1,
-				createdAt: new Date(),
-				updatedAt: new Date()
-			},
-			{
-				userId: 2,
-				recipeId: 1,
-				favorited: 0,
-				createdAt: new Date(),
-				updatedAt: new Date()
-			}
-		];
-
-		return queryInterface.bulkInsert("favorites", favorites, {});
+				updatedAt: new Date(),
+			};
+		});
+		return queryInterface.bulkInsert('favorites', favorites, {});
 	},
 
 	down: (queryInterface, Sequelize) => {
-		return queryInterface.bulkDelete("favorites", null, {});
-	}
+		return queryInterface.bulkDelete('favorites', null, {});
+	},
 };
